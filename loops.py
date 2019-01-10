9d3d3ab29aadd24628a0655720739fc5d64abfdf
fali0 <ofirasathali@gmail.com>
2019-01-09 19:37:59 -0600

    python tutorial

---
+++ loops.py
@@ -0,0 +1,20 @@
+#comment CTRL/
+
+# for i in range(10):
+#     print(i)
+
+# while (True):
+#     str = input("Please say something, quit to exit from loop:")
+#     if str == "quit":
+#         break
+#     print(str)
+
+while ( True ):
+    str = input("Please say something, quit to exit from loop:")
+    if str == "quit":
+        break
+    print(str)
+
+print("reached end of program")
+
+
