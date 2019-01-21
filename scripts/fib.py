621f27b96bd27132d09d8d010b5f30ca9112b540
fali0 <felsewhere1@gmail.com>
2019-01-21 14:59:13 -0600

    fibonacci

---
+++ scripts/fib.py
@@ -0,0 +1,4 @@
+import fibonacci as f
+
+print (__name__)
+f.fib(20)
--- scripts/fibonacci.py
+++ scripts/fibonacci.py
@@ -1,42 +1,34 @@
-'''Fibonacci iterator'''
+# fibonnaci function 
 
-class Fib:
-    '''iterator that yields numbers in the Fibonacci sequence'''
+def fib(n):
+	''' prints out a list of n fibonnaci numbers
+	
+	Args:
+	parm1: n  is the number of fibonnaci numbers to print
 
-    def __init__(self, max):
-        self.max = max
+	Returns:
+	nothing
+	'''
+	a, b = 0, 1              #Note multiple assignment!
+	counter = 1
+	while counter < n:
+		print (a, end=' ')
+		a, b = b, a+b
+		print()
+		counter += 1
 
-    def __iter__(self):
-        self.a = 0
-        self.b = 1
-        return self
+	print(__name__)
+	return(0)
 
-    def __next__(self):
-        fib = self.a
-        if fib > self.max:
-            raise StopIteration
-        self.a, self.b = self.b, self.a + self.b
-        return fib
+################################################# #################################################
+# main
+# every python program has special variable that look like
+# __xyz__. __name__ is one of them and has the name of the module or __main__ if it is run as a program
 
-# Copyright (c) 2009, Mark Pilgrim, All rights reserved.
-# 
-# Redistribution and use in source and binary forms, with or without modification,
-# are permitted provided that the following conditions are met:
-# 
-# * Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+if __name__ == "__main__":
+	n = int(input("Please enter a positive number: "))
+
+	if n >= 0:
+		fib(n)
+	else:
+		print("Something went wrong")
